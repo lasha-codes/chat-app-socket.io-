d6c0a300ba47a6c6b3930d3ad3833eff162eccfb
Lasha Magaldadze <lashagiorgi420@gmail.com>
2024-08-01 17:30:01 +0400

    having setup socket.io on front and rn just playing around with things

--- client/src/components/Chat/Chat.jsx
+++ client/src/components/Chat/Chat.jsx
@@ -1,4 +1,26 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable react/prop-types */
+import { useState, useEffect } from 'react'
+import queryString from 'query-string'
+import io from 'socket.io-client'
+
 const Chat = () => {
+  const [name, setName] = useState('')
+  const [room, setRoom] = useState('')
+  const ENDPOINT = 'localhost:5000'
+
+  useEffect(() => {
+    const { name, room } = queryString.parse(window.location.search)
+    const socket = io(ENDPOINT)
+
+    setName(name)
+    setRoom(room)
+
+    socket.emit('join', { name, room }, ({ error }) => {
+      alert(error)
+    })
+  }, [ENDPOINT, window.location.search])
+
   return <div>Chat</div>
 }
 
--- server/index.js
+++ server/index.js
@@ -1,23 +1,36 @@
 const express = require('express')
-const socket_io = require('socket.io')
+const { Server } = require('socket.io')
 const http = require('http')
+const cors = require('cors')
 
 const PORT = process.env.PORT || 5000
 
 const router = require('./router')
 
 const app = express()
+
+app.use(cors())
+app.use(router)
+
 const server = http.createServer(app)
-const io = socket_io(server)
+const io = new Server(server, { cors: { origin: 'http://localhost:5173' } })
 
 io.on('connection', (socket) => {
   console.log('We have a new connection')
+
+  socket.on('join', ({ name, room }, callback) => {
+    console.log(name, room)
+
+    const error = true
+
+    if (error) {
+      callback({ error: 'error' })
+    }
+  })
 
   socket.on('disconnect', () => {
     console.log('User has been disconnected')
   })
 })
-
-app.use(router)
 
 server.listen(PORT, () => console.log('Server is running on port', PORT))
